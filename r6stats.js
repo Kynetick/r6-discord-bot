f50daffc2e4a8b5725133fa7416111580c7e6f06
Kynetick <adri.glmt@gmail.com>
2022-04-02 15:52:52 +0200

    Added some information to the embed message : rank + MMR

--- r6stats.js
+++ r6stats.js
@@ -2,6 +2,7 @@ const {prefix} = require("./config.json");
 require('dotenv').config();
 const R6API = require('r6api.js').default;
 const { MessageEmbed } = require('discord.js');
+const {size} = require("ffmpeg/lib/presets");
 
 const { UBI_EMAIL: email = '', UBI_PASSWORD: password = '' } = process.env;
 const r6api = new R6API({ email, password });
@@ -18,39 +19,59 @@ module.exports.stats = async (message) => {
         const platform = 'uplay'; //if you want another value, check the doc : https://github.com/danielwerg/r6api.js#definitions
 
         const { 0: player } = await r6api.findByUsername(platform, username);
-        if (!player) return 'Player not found';
+            if (!player) return 'Player not found';
 
         const { 0: stats } = await r6api.getStats(platform, player.id);
-        if (!stats) return 'Stats not found';
+            if (!stats) return 'Stats not found';
         const { pvp: { general } } = stats;
 
         const { 0: rank } = await r6api.getRanks(platform, player.id, { regionIds: 'emea', boardIds: 'pvp_ranked' });
-        if (!rank) return;
+            if (!rank) return;
         const { seasons: { 25: { regions: { emea: { boards: { pvp_ranked }} } } } } = rank;
-        console.log(pvp_ranked);
+        //console.log(pvp_ranked);
 
         const { 0: level } = await r6api.getProgression(platform, player.id);
 
-        const exampleEmbed = new MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle(`${player.username}`)
-            .setURL('https://discord.js.org/')
-            .setDescription('Some description here')
-            .setThumbnail('https://i.imgur.com/AfFp7pu.png')
-            .addFields(
-                { name: 'h', value: 'h' },
-                { name: '\u200B', value: '\u200B' },
-                { name: 'Level', value: `${level.level}`, inline: true },
-                { name: 'K/D', value: `${rank.seasons["25"].regions.emea.boards.pvp_ranked.kd}`, inline: true },
-            )
-            .setImage('https://i.imgur.com/AfFp7pu.png')
-            .setTimestamp()
+        const exampleEmbed = new MessageEmbed({
+            color: 0x0099ff,
+            //title: 'Some title',
+            //url: 'https://discord.js.org',
+            author: {
+                name: `${player.username}`,
+                icon_url: `${player.avatar["146"]}`,
+                //url: 'https://discord.js.org',
+            },
+            description: `${player.username}'s stats for current season`,
+            thumbnail: {
+                url: `${rank.seasons["25"].regions.emea.boards.pvp_ranked.current.icon}`,
+            },
+            fields: [
+                {
+                    name: 'Level', value: `${level.level}`, inline: true,
+                },
+                {
+                    name: 'K/D', value: `${rank.seasons["25"].regions.emea.boards.pvp_ranked.kd}`, inline: true,
+                },
+            ],
+            fields: [
+                {
+                    name: 'MMR', value: `${rank.seasons["25"].regions.emea.boards.pvp_ranked.current.name}`, inline: true,
+                },
+                {
+                    name: 'MMR', value: `${rank.seasons["25"].regions.emea.boards.pvp_ranked.current.mmr}`, inline: true,
+                }
+            ],
+            timestamp: new Date(),
+            footer: {
+                text: 'Kynetick 2022',
+                //icon_url: 'https://i.imgur.com/AfFp7pu.png',
+            },
+        })
 
-        console.log('Stats displayed');
-        return message.channel.send({ embeds: [exampleEmbed] });
+    console.log('Stats displayed');
+    return message.channel.send({ embeds: [exampleEmbed] });
 
     } else if (message.content.startsWith(prefix) && (!message.author.bot)) { //checks if the message starts with the prefix and was not sent by bot
         message.channel.send("You need to enter a valid command !");
     }
-
 };
